4a5f2ca5f627c0ecdf1baa0ee1dfcd91854eaa55
gmmkmtgk <keshavmaheshwari800@gmail.com>
2025-01-29 17:02:35 +0530

    Import correction Prod Error

--- src/pages/portfolio/momentumStocks.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MainCard from 'components/MainCard';
-
-const MomentumStocks = () => {
-  return <MainCard title="Momentum Stocks"></MainCard>;
-};
-
-export default MomentumStocks;
