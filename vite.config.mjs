24bb4ab2a579cf345557b9ce175d7764d905dcbf
gmmkmtgk <keshavmaheshwari800@gmail.com>
2025-01-29 21:46:57 +0530

    vite build config removal

--- vite.config.mjs
+++ vite.config.mjs
@@ -29,17 +29,5 @@ export default defineConfig({
   preview: {
     open: true, // this ensures that the browser opens upon preview start
     port: 3000 // this sets a default port to 3000
-  },
-  build: {
-    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
   }
 });
